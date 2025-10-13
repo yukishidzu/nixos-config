4801e9a042772b8d6b2c711f85725bc3e181875a
Ilya <45292311+yukishidzu@users.noreply.github.com>
2025-10-13 04:00:50 +0300

    feat(home): add programs-new.nix entry consolidating modules and base packages

---
+++ home/programs-new.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, lib, inputs, pkgs-unstable, ... }:
+
+let
+  hasCursor = pkgs-unstable ? cursor;
+  hasSpotify = pkgs ? spotify;
+  hasVSCodium = pkgs-unstable ? vscodium;
+  opt = lib.optionals;
+in
+{
+  # Импорт модулей
+  imports = [
+    ../hyprland-new.nix
+    ../wlogout-new.nix
+  ];
+
+  # Базовые пакеты
+  home.packages =
+    (with pkgs; [
+      telegram-desktop
+      vlc
+      file-roller
+      pavucontrol
+      nodejs
+      python3
+      rustc
+      cargo
+      go
+    ])
+    ++ (opt hasSpotify [ pkgs.spotify ])
+    ++ (opt hasCursor [ pkgs-unstable.cursor ])
+    ++ (opt hasVSCodium [ pkgs-unstable.vscodium ]);
+
+  # XDG и mime
+  xdg = {
+    enable = true;
+    mimeApps.enable = true;
+  };
+
+  # Не управляем Firefox через HM
+  programs.firefox.enable = false;
+}
